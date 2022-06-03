c81e410c089a58dfbe65357044214238678e8c4c
raphael-vi <raphaelb.menezess@gmail.com>
2022-06-03 13:45:02 -0300

    Mudando o tamanho do grid
    Adicionei o personalizador de grid com um slider.
    Problemas:  A classe .active (muda a cor quando o mouse passa por cima) não está sendo adicionada quando o tamanho do grid é alterado

--- script.js
+++ script.js
@@ -1,25 +1,70 @@
-const container = document.querySelector('.grid-container')
-const gridCell = document.createElement('div');
-gridCell.classList.add('grid-cell');
+const DEFAULT_GRID = 16;
+let size = DEFAULT_GRID;
+const container = document.querySelector('.grid-container');
+
+const clear = document.querySelector('.clear');
+const gridSize = document.querySelector('.gridsize')
+const submit = document.querySelector('.submit')
+
+gridSize.addEventListener('change', (e) => sizeGrid(e.target.value));
+
+gridSize.min='1';
+gridSize.max='100';
+
+
+
 
 function addGrid(number){
     let mult = number*number
     container.style.gridTemplateRows = `repeat(${number}, 1fr)`
     container.style.gridTemplateColumns = `repeat(${number}, 1fr)`
+
+
+
     for(let i = 0; i <mult; i++){
-        container.appendChild(gridCell.cloneNode())
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        container.appendChild(gridCell)
         
     }
 }
 
-addGrid(6)
+function sizeGrid(number){
+    newSize(number)
+    reload()
+}
 
-const cell = document.querySelectorAll('.grid-cell')
+function reload(){
+    container.textContent = ''
+    addGrid(size)
+}
+
+
+function newSize(current){
+    size = current
+}
+
+
+
+sizeGrid(size)
+
+
+/*------EVENTOS --- */
+let cell = document.querySelectorAll('.grid-cell')
+
+
 
 cell.forEach(cell =>{ 
-    cell.addEventListener( 'mouseover' , () => { 
+    cell.addEventListener( "mouseover" , () => { 
         cell.classList.add('active')
     })
 })
 
+
+
+clear.addEventListener('click', ()=>{ 
+    cell.forEach(cell =>{ 
+       cell.classList.remove('active')
+    })
+})
 
